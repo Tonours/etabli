675e0428c546d84cc7ffd022f2702840b5bb890b
Anthony Guimard <bonjour@anthonyguimard.fr>
2026-02-18 23:18:18 +0100

    refactor(nvim): fix install colorscheme, update catppuccin, clean options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,30 +1,22 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Only overrides of LazyVim defaults: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
 local opt = vim.opt
 
 -- UI
-opt.relativenumber = true
 opt.colorcolumn = "100"
-opt.scrolloff = 8
+opt.scrolloff = 8 -- LazyVim default is 4
 opt.sidescrolloff = 8
 
 -- Tabs
 opt.tabstop = 2
 opt.shiftwidth = 2
-opt.expandtab = true
 
 -- Auto-reload files (for external tools like OpenCode)
 opt.autoread = true
 
--- Persistent undo history
-opt.undofile = true
-
 -- No line wrapping
 opt.wrap = false
-
--- Always show sign column (prevents layout shift)
-opt.signcolumn = "yes"
 
 -- Faster updates (for CursorHold events, git signs, etc.)
 opt.updatetime = 250
@@ -34,7 +26,3 @@ opt.timeoutlen = 300
 
 -- Better completion experience
 opt.completeopt = "menu,menuone,noselect"
-
--- Split behavior
-opt.splitright = true
-opt.splitbelow = true
