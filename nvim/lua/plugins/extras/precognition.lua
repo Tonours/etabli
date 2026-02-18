9bbb877315d2b0e4e3e26aaf92140f284d138ddf
Anthony Guimard <bonjour@anthonyguimard.fr>
2026-02-18 23:16:13 +0100

    fix(nvim): remove dead explorer.lua and fix precognition return format

--- nvim/lua/plugins/extras/precognition.lua
+++ nvim/lua/plugins/extras/precognition.lua
@@ -1,13 +1,15 @@
 -- Precognition: shows virtual text hints for motions
 return {
-  "tris203/precognition.nvim",
-  event = "VeryLazy",
-  opts = {
-    startVisible = true,
-    showBlankVirtLine = true,
-    highlightColor = { link = "Comment" },
-  },
-  keys = {
-    { "<leader>up", function() require("precognition").toggle() end, desc = "Toggle Precognition" },
+  {
+    "tris203/precognition.nvim",
+    event = "VeryLazy",
+    opts = {
+      startVisible = true,
+      showBlankVirtLine = true,
+      highlightColor = { link = "Comment" },
+    },
+    keys = {
+      { "<leader>up", function() require("precognition").toggle() end, desc = "Toggle Precognition" },
+    },
   },
 }
