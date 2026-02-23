fd19c93b899883d4f47171b9ec51b94d147acc20
Anthony Guimard <bonjour@anthonyguimard.fr>
2026-02-23 22:37:42 +0100

    fix(sketchybar): detect WiFi interface dynamically, set topmost=on

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -3,7 +3,8 @@
 source "$CONFIG_DIR/icons.sh"
 source "$CONFIG_DIR/colors.sh"
 
-WIFI_SSID=$(ipconfig getsummary en0 2>/dev/null | awk -F ' SSID : ' '/ SSID : / {print $2}')
+WIFI_IF=$(networksetup -listallhardwareports 2>/dev/null | awk '/Wi-Fi/{getline; print $2}')
+WIFI_SSID=$(ipconfig getsummary "${WIFI_IF:-en0}" 2>/dev/null | awk -F ' SSID : ' '/ SSID : / {print $2}')
 
 if [ -n "$WIFI_SSID" ]; then
     ICON="$ICON_WIFI"
